f601453ff114e7a6d22dc45ebb69b6c6355969c7
realshade <realshade@nafanya.me>
2025-03-18 12:10:59 +0200

    видалення непотрібного

--- .dev/docker/config/sql/mysql.init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE `$DB_NAME` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-
-create user '$DB_USER'@'%' identified by '$DB_PASS';
-grant super on *.* to '$DB_USER'@'%';
-grant all privileges on *.* to '$DB_USER'@'%';
-flush privileges;
