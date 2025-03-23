b40c8e8187bfd98ffa06d272762d694591061a93
realshade <realshade@nafanya.me>
2025-03-23 16:10:44 +0200

    refactor: reorganize API routes and update TaskController namespace

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\Print;
+namespace App\Http\Controllers\Api;
 
 use App\Enums\TaskStatus;
 use App\Http\Controllers\Controller;
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\Print\TaskController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth.api_token')->group(function() {
-    Route::prefix('print')->group(function() {
-        Route::get('tasks', [TaskController::class, 'index']);
-        Route::get('tasks/{task}', [TaskController::class, 'show']);
-        Route::match(['get', 'post'], 'print-start', [TaskController::class, 'beforePrint']);
-        Route::match(['get', 'post'], 'print-end', [TaskController::class, 'afterPrint']);
-    });
+    Route::get('tasks', [TaskController::class, 'index']);
+    Route::get('tasks/{task}', [TaskController::class, 'show']);
+    Route::match(['get', 'post'], 'print-start', [TaskController::class, 'beforePrint']);
+    Route::match(['get', 'post'], 'print-end', [TaskController::class, 'afterPrint']);
 });
