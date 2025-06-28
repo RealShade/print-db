5c6f7778fa128f987431f9c856e16c1fb556fee1
realshade <realshade@nafanya.me>
2025-06-28 16:09:48 +0300

    додано відображення назви філамента у списку витрат для покращення інформаційності

--- resources/views/statistics/income.blade.php
+++ resources/views/statistics/income.blade.php
@@ -70,7 +70,7 @@
                                                                 @foreach($spool->filament->colors as $color)
                                                                     <span class="filament-color-preview" style="background-color: {{ $color }}; width: 20px; height: 20px; display: inline-block;"></span>
                                                                 @endforeach
-                                                                {{ $spool->filament->vendor->name }}, {{ $spool->filament->type->name }}, {{ $spool->packaging->name }}
+                                                                {{ $spool->filament->vendor->name }}, {{ $spool->filament->type->name }}, {{ $spool->filament->name }}, {{ $spool->packaging->name }}
                                                             </td>
                                                             <td class="text-end">{{ number_format($spool->weight_initial, 1) }}</td>
                                                             <td class="text-end">{{ number_format($spool->cost, 2) }}</td>
